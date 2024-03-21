92272c9f99b1c1b52dda0e86e6458cd65a3ab323
Mariana De Veras <mariana.de.veras@eaportal.org>
2024-03-21 11:28:35 -0300

    certo agora

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const botaoAbrir = document.querySelector(".header > button")
-const botaoFechar = document.querySelector(".header nav button")
+const botaoAbrir = document.querySelector("header > button");
+const botaoFechar = document.querySelector("header nav button");
 
-const body = document.querySelector("body")
-const nav = document.querySelector("header .mobile")
-
+const body = document.querySelector("body");
+const nav = document.querySelector("header .mobile");
 
 botaoAbrir.addEventListener('click', abrirMenu)
 botaoFechar.addEventListener('click', fecharMenu)
 
 function abrirMenu() {
-    body.classList.add("escurecer")
-    nav.classList.add("abrir")
+    body.classList.add("escurecer");
+    nav.classList.add("abrir");
+}
+function fecharMenu() {
+    body.classList.remove("escurecer");
+    nav.classList.remove("abrir");
 }
 
-function fecharMenu() {
-    body.classList.remove("escurecer")
-    nav.classList.remove("abrir")
-}
